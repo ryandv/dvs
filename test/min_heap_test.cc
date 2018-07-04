8c4dfb1915291065cf3e87a2b47b51eb39e15696
Ryan DV <rdevilla@uwaterloo.ca>
2018-07-04 01:39:10 -0400

    Import min heap implementation from C project

---
+++ test/min_heap_test.cc
@@ -0,0 +1,49 @@
+#include "gtest/gtest.h"
+
+#include "dvs/min_heap.h"
+
+namespace {
+    class MinHeapTest : public ::testing::Test {
+        protected:
+            virtual void SetUp()
+            {
+            }
+    };
+}
+
+TEST_F(MinHeapTest, min_heapify_left_subtree)
+{
+    int h[] = { 3, 1, 2 };
+    min_heapify_array(3, &h[0], 0);
+    ASSERT_EQ(h[0], 1);
+    ASSERT_EQ(h[1], 3);
+    ASSERT_EQ(h[2], 2);
+}
+
+TEST_F(MinHeapTest, min_heapify_array_right)
+{
+    int h[] = { 3, 2, 1 };
+    min_heapify_array(3, &h[0], 0);
+    ASSERT_EQ(h[0], 1);
+    ASSERT_EQ(h[1], 2);
+    ASSERT_EQ(h[2], 3);
+}
+
+TEST_F(MinHeapTest, min_heapify_array_recursive)
+{
+    int h[] = { 10, 1, 999, 2, 3 };
+    min_heapify_array(4, &h[0], 0);
+    ASSERT_EQ(h[0], 1);
+    ASSERT_EQ(h[1], 2);
+    ASSERT_EQ(h[2], 999);
+    ASSERT_EQ(h[3], 10);
+    ASSERT_EQ(h[4], 3);
+}
+
+TEST_F(MinHeapTest, build_min_heap_singleton)
+{
+    int input[] = { 1 };
+    int *built_heap = build_min_heap(1, &input[0]);
+    ASSERT_EQ(built_heap[0], 1);
+    free(built_heap);
+}
