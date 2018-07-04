6c18262346f715313305cf2f2596e2e5b44953e8
Ryan DV <rdevilla@uwaterloo.ca>
2018-07-04 01:43:30 -0400

    Fix include guard

--- src/include/dvs/min_heap.h
+++ src/include/dvs/min_heap.h
@@ -1,8 +1,9 @@
 #ifndef __MIN_HEAP_H__
 #define __MIN_HEAP_H__
-#endif
 
 #include <stdio.h>
 
 void min_heapify_array(int len, int *heap_as_array, int root_index);
 int *build_min_heap(int num_elements, int *elements);
+
+#endif
